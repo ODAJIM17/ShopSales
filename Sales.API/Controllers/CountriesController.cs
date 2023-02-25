6e2a8d161e02e5e774cd6a5beadbb13e71c9edc3
Jimmy Davila <davila56jd@gmail.com>
2023-02-25 11:59:11 -0500

    AddedDbSeeder

--- Sales.API/Controllers/CountriesController.cs
+++ Sales.API/Controllers/CountriesController.cs
@@ -42,18 +42,48 @@ namespace Sales.API.Controllers
         [HttpPost]
         public async Task<ActionResult> PostAsync(Country country)
         {
-            _context.Add(country);
-            await _context.SaveChangesAsync();
-            return Ok(country);
+            try
+            {
+                _context.Add(country);
+                await _context.SaveChangesAsync();
+                return Ok(country);
+            }
+            catch (DbUpdateException dbUpdateException)
+            {
+                if(dbUpdateException.InnerException!.Message.Contains("duplicate"))
+                {
+                    return BadRequest("This country already exists");
+                }
+                return BadRequest(dbUpdateException.Message);
+            }
+            catch(Exception exception)
+            {
+               return BadRequest(exception.Message);  
+            }
         }
 
 
         [HttpPut]
         public async Task<ActionResult> PutAsync(Country country)
         {
-            _context.Update(country);
-            await _context.SaveChangesAsync();
-            return Ok(country);
+            try
+            {
+                _context.Update(country);
+                await _context.SaveChangesAsync();
+                return Ok(country);
+            }
+            catch (DbUpdateException dbUpdateException)
+            {
+                if (dbUpdateException.InnerException!.Message.Contains("duplicate"))
+                {
+                    return BadRequest("This country already exists");
+                }
+                return BadRequest(dbUpdateException.Message);
+            }
+            catch (Exception exception)
+            {
+                return BadRequest(exception.Message);
+            }
         }
 
 
